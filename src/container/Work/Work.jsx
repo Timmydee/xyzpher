f517eb0a919a0a8270bc991fca7ec98f7bd8db40
Timmydee <dahunsitimmy@gmail.com>
2023-05-15 20:09:32 +0100

    xyzpher

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -6,10 +6,8 @@ const Work = () => {
   return (
     <div className='works'> 
       <div className='work'>
-        {/* <div className='work__col'>How does it work?</div> */}
         <img className='work__col' src={how} />
         <img className='work__col style={{animation: 10s, margin-right: 0.6rem}}' src={how} />
-        {/* <div className='work__col style={{animation: 10s}}'>How does it work?</div> */}
       </div>
     </div>
     
